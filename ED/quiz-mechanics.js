f6da122e51ebb1e082ce0ad0bc21b3f1e0097256
FrozenLake01 <147809859+FrozenLake01@users.noreply.github.com>
2024-01-11 17:03:50 +0800

    modifying files

--- ED/quiz-mechanics.js
+++ ED/quiz-mechanics.js
@@ -1,126 +1,165 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const questionButtons = document.querySelector("#answer-buttons btn");
 
-const startButton = document.getElementById('start-btn')
+let shuffledQuestions, currentQuestionIndex;
 
-
-
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-
-let shuffledQuestions, currentQuestionIndex
-
-// startButton.addEventListener('click', startGame)
-// nextButton.addEventListener('click', () => {
-//   currentQuestionIndex++
-//   setNextQuestion()
-// })
-
-playBtn.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
-  // startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
 }
 
 function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct;
     }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
 }
 
 function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
-  questionContainerElement.style.backgroundColor = "white";
 }
 
 function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    nextButton.classList.remove("hide");
   } else {
-    // startButton.innerText = 'Restart'
-    // startButton.classList.remove('hide')
-    
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
   }
 }
 
 function setStatusClass(element, correct) {
-  clearStatusClass(element)
+  clearStatusClass(element);
   if (correct) {
-    questionContainerElement.style.backgroundColor = "green";
+    element.classList.add("correct");
+    element.disabled = true;
+    console.log("correct");
   } else {
-    questionContainerElement.style.backgroundColor = "red";
+    element.classList.add("wrong");
+    element.disabled = true;
+    console.log("correct");
   }
 }
 
 function clearStatusClass(element) {
-//   element.classList.remove('correct')
-//   element.classList.remove('wrong')
-questionContainerElement.style.backgroundColor = "white";
-
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
 }
-
 const questions = [
   {
-    question: 'What is 2 + 2?',
+    question: "What does HTML stand for",
     answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false }
-    ]
+      { text: "Hypertext Making Language", correct: false },
+      { text: "Hyper Transfer Markup Language", correct: false },
+      { text: "Hypertext Markup Language", correct: true },
+      { text: "Hyper Transfer Making Language", correct: false },
+    ],
   },
   {
-    question: 'Who is the best YouTuber?',
+    question: "A <tag>",
     answers: [
-      { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
-      { text: 'Dev Ed', correct: true },
-      { text: 'Fun Fun Function', correct: true }
-    ]
+      { text: "Is used to divide sections", correct: false },
+      { text: "Is not needed in HTML", correct: false },
+      { text: "Tells the browser how to format our content", correct: true },
+      { text: "Is part of the title line in HTML", correct: false },
+    ],
   },
   {
-    question: 'Is web development fun?',
+    question: "<h> is",
     answers: [
-      { text: 'Kinda', correct: false },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: false },
-      { text: 'IDK', correct: false }
-    ]
+      { text: "Headline Tag", correct: false },
+      { text: "Header Tag", correct: true },
+      { text: "Half of a tag", correct: false },
+      { text: "Closing header tag", correct: false },
+    ],
   },
   {
-    question: 'What is 4 * 2?',
+    question: "</h> is",
     answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
-    ]
-  }
-]
+      { text: "Headline Tag", correct: false },
+      { text: "Header Tag", correct: false },
+      { text: "Half of a tag", correct: false },
+      { text: "Closing header tag", correct: true },
+    ],
+  },
+  {
+    question: 'An " Empty Tag" contains',
+    answers: [
+      { text: "Break Tag", correct: false },
+      { text: "Both Starting Tag and Ending Tag", correct: false },
+      { text: "Only Starting Tag", correct: true },
+      { text: "Only Ending Tag", correct: false },
+    ],
+  },
+  {
+    question: "Fundamental HTML Block is known as ",
+    answers: [
+      { text: "HTML Body", correct: false },
+      { text: "HTML Tag", correct: false },
+      { text: "HTML Attribute", correct: true },
+      { text: "HTML Element", correct: false },
+    ],
+  },
+  {
+    question: "All HTML Tags are enclosed in",
+    answers: [
+      { text: "# and #", correct: false },
+      { text: "? and !", correct: false },
+      { text: "< and />", correct: true },
+      { text: "{ and }", correct: false },
+    ],
+  },
+  {
+    question: "This Language ____ was developed by ______",
+    answers: [
+      { text: "Hyper Text Mark up Language, Laudon", correct: false },
+      { text: "Hyper Text Make up Language, Tim Berners Lee", correct: false },
+      { text: "Hyper Text Mark Up Language, Tim Berners Lee", correct: true },
+      { text: "Hyper Text Mark up Language, Lauv", correct: false },
+    ],
+  },
+  {
+    question: 'A "Container Tag" in HTML has',
+    answers: [
+      { text: "Only Ending Tag", correct: false },
+      { text: "? and !", correct: false },
+      { text: "< and />", correct: true },
+      { text: "{ and }", correct: false },
+    ],
+  },
+];
